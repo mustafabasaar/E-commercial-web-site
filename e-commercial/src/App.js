1ae32bc5c42ed8abfd3d59776b89dda9aceb0888
mustafabasar96 <mustafabasar96@hotmail.com>
2023-11-23 21:52:44 +0300

    header layout completed

--- e-commercial/src/App.js
+++ e-commercial/src/App.js
@@ -1,9 +1,11 @@
 import Footer from "./Layouts/Footer";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <>
       <div className="w-screen">
+        <HomePage />
         <Footer />
       </div>
     </>
--- e-commercial/src/Layouts/Header.js
+++ e-commercial/src/Layouts/Header.js
@@ -1,3 +1,72 @@
+import React from "react";
+
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Header() {
-  return <div></div>;
+  return (
+    <>
+      <div className="bg-[#252B42] py-3 px-[24px] text-white text-[14px] font-bold flex flex-wrap justify-between font-[Montserrat] gap-y-2">
+        <div className="flex gap-4 items-center mr-4">
+          <div className="flex items-center gap-2">
+            <FontAwesomeIcon
+              icon={faPhone}
+              style={{ color: "#ffffff", width: "16px" }}
+            />
+            <p>(225) 555-0118</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              style={{ color: "#ffffff", width: "16px" }}
+            />
+            <p>michelle.rivera@example.com</p>
+          </div>
+        </div>
+        <div className="flex mr-4">
+          <p>Follow Us and get a chance to win 80% off</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <p>Follow Us : </p>
+          <a href="#">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon
+              icon={faYoutube}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Header;
